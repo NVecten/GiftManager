e72c0ac016aa26fdebc7e757e808566416ffe1f7
Vít Urban <p.kvasnovska@gmail.com>
2019-12-08 15:55:13 +0100

    zprovozněné překlikávání stránek

--- giftmanager/giftmanager/views.py
+++ giftmanager/giftmanager/views.py
@@ -6,7 +6,7 @@ class Index(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        return {"ahoj":Person.objects.get(name="maminka")}
+        return {"ahoj":Person.objects.get(name="Albert")}
 
 class Names(TemplateView):
     template_name = "names.html"
