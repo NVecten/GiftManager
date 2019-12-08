ca5a3042c6f93b1f69c1522fa6936d4679b20cbd
globalista <globalista@seznam.cz>
2019-12-08 16:24:13 +0100

    odstranena chyba ve views

--- giftmanager/giftmanager/views.py
+++ giftmanager/giftmanager/views.py
@@ -6,11 +6,12 @@ class Index(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        return {"ahoj":Person.objects.get(name="maminka")}
+        return {"ahoj":Person.objects.all()}
 
 class Names(TemplateView):
     template_name = "names.html"
 
     def get_context_data(self, **kwargs):
         return {"object":Person.objects.all()}
+
 
