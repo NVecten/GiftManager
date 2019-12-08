fc42247bc5f806a7f8700780e64c9ab891311175
globalista <globalista@seznam.cz>
2019-12-08 17:52:57 +0100

    vypise jmena pod sebe

--- giftmanager/giftmanager/views.py
+++ giftmanager/giftmanager/views.py
@@ -1,12 +1,22 @@
 from django.views.generic import TemplateView
 
-from .models import Person
+from .models import Person, GiftToPerson
 
 class Index(TemplateView):
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
-        return {"ahoj":Person.objects.all()}
+
+        for person in {"persons":Person.objects.all()}:
+
+
+
+            '''
+            for gift in {"gifts": GiftToPerson.objects.filter(person__id = person.id )}:
+            pass
+            '''
+
+        return {"persons":Person.objects.all()}
 
 class Names(TemplateView):
     template_name = "names.html"
