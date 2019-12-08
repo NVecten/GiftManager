e7e456a61b8de8bf562686bdd8b06d32ef19dc0e
globalista <globalista@seznam.cz>
2019-12-08 15:07:50 +0100

    pridany gifts, gift price

--- giftmanager/giftmanager/admin.py
+++ giftmanager/giftmanager/admin.py
@@ -2,3 +2,4 @@ from django.contrib import admin
 from .models import Person, Gift, GiftToPerson
 
 admin.site.register(Person)
+admin.site.register(Gift)
--- giftmanager/giftmanager/models.py
+++ giftmanager/giftmanager/models.py
@@ -2,11 +2,10 @@ from django.db import models
 
 class Person(models.Model):
   name = models.CharField(max_length=100)
-  class Meta:
-    app_label = 'giftmanager'
 
 class Gift(models.Model):
   gift = models.CharField(max_length=100)
+  price = models.FloatField()
 
 class GiftToPerson(models.Model):
   person_id = models.ForeignKey(Person, on_delete=models.CASCADE)
